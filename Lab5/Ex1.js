282c7c8510967cfa8de8c99bcc16b1a05b78fd36
JonathanMachado44 <jam30@hawaii.edu>
2023-02-04 22:03:27 -1000

    c
    c

---
+++ Lab5/Ex1.js
@@ -0,0 +1,9 @@
+age =  20;
+fav_num = 3;
+day_of_birth = 6;
+month_of_birth = 7;
+console.log(
+    age + fav_num / day_of_birth * month_of_birth);
+
+    console.log(
+        (age + fav_num) / day_of_birth * month_of_birth);

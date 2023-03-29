10dec7f916a30320f79d91f956f53c61855404ac
JonathanMachado44 <jam30@hawaii.edu>
2023-03-28 22:49:09 -1000

    1
    1

---
+++ Lab12/info_server_Ex2a.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+//respond to any req for any path
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+app.get('/test', function (request, response, next) {
+    response.send(request.method + ' to paths ' + request.path);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, () => console.log(`listening on port 8080`));
---
+++ WODS/Store1/products_data.js
@@ -0,0 +1,34 @@
+var products = [
+    {
+    //Prod 1
+    brand: "HTC",
+    price: 40.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+    },
+    {
+    //Prod 2
+    brand: "Apple",
+    price: 75.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+    },
+    {
+    //Prod 3
+        brand: "Nokia",
+        price: 35.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+    },
+    {
+    //Prod 4
+        brand: "Samsung",
+        price: 45.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+    },
+    {
+    //Prod 5
+    brand: "Blackberry",
+    price: 10.00,
+    image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+    }
+]
+
+console.log("Product variables loaded...")

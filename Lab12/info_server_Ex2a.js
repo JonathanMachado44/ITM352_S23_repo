dec4305bc9ae302821559df46bfab6cf7d359775
JonathanMachado44 <jam30@hawaii.edu>
2023-04-04 04:29:35 -1000

    2
    2

--- Lab12/info_server_Ex2a.js
+++ Lab12/info_server_Ex2a.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-//respond to any req for any path
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path);
+app.get('/test', function (request, response, next) {
+    console.log("Got a test path");
     next();
 });
-app.get('/test', function (request, response, next) {
-    response.send(request.method + ' to paths ' + request.path);
+
+app.all('*', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path);
 });
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(8080, () => console.log(`listening on port 8080`));
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+// app.all - will respond to wither get or post 
+// * - every path 
+// rule action with set of pairs - can put any as many rules 
+// server can respond to request 

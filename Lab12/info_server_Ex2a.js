e464a0f21d23ee289b70c18ea89d642a8186a951
JonathanMachado44 <jam30@hawaii.edu>
2023-04-04 12:37:24 -1000

    2
    2

--- Lab12/info_server_Ex2a.js
+++ Lab12/info_server_Ex2a.js
@@ -10,7 +10,7 @@ app.all('*', function (request, response, next) {
     response.send(request.method + ' to path ' + request.path);
 });
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
-// app.all - will respond to wither get or post 
+// app.all - will respond to wether get or post 
 // * - every path 
 // rule action with set of pairs - can put any as many rules 
 // server can respond to request 

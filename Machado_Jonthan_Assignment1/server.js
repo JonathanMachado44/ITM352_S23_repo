bdb86fc57fce4574a172ca60e9d9e363ae441e3a
JonathanMachado44 <jam30@hawaii.edu>
2023-04-09 12:29:14 -1000

    Update server.js
    1

--- Machado_Jonthan_Assignment1/server.js
+++ Machado_Jonthan_Assignment1/server.js
@@ -1,120 +1,76 @@
-//Combination of Kazmans Lab 13 Explantion, Daniel Ports input, and a culmination of some WODs
-//I start page by going to the server 
-var express = require('express'); //code for server
-var qs = require('querystring');
-var app = express();
-var products = require('./products.json');
+var express = require('express'); // importing the express file from node_modules
+var app = express(); //putting imported express files into function named app
+var products = require('./product_data.json');
+// Routing 
 
-//Krizel Tomines and Daniel Port
-app.use(express.urlencoded({ extended: true })); //decode URL encoded data from POST requests
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
-products.forEach( (prod,i) => {prod.total_sold = 0}); 
-
-app.get("/products.js", function (request, response, next) {
-    response.type('.js');
-    var products_str = `var products = ${JSON.stringify(products)};`;
-    response.send(products_str);
-});
-//retreived from Nate Moylan
-    //this function creates a for loop to generate the products for the page
-    function display_products() {
-        str = '';
-        // loop to generate the products
-        for (i = 0; i < products.length; i++) {
-            str += `
-            <section class ="item">
-            <h2>${products[i].name}</h2> 
-            <h3 label id ="quantity_available${i}"><i>Available: ${products[i].quantity_available} available!</i></h3></label>
-            <h4>$${products[i].price.toFixed(2)}</h4>
-            <img src="/images/${products[i].image}" class="img">
-            <label id ="quantity${i}_label">Number of Items: </label>
-            <input type="text" placeholder="0" name="quantity${i}" onkeyup="checkQuantityTextbox(this);"> 
-            </section>`;
-
-            // makes sure the quantity inputted by the user is validated. 
-            if (typeof req.query['purchasebutton'] != 'undefined') {
-        
-                for (i = 0; i < products.length; i++) {
-                    if (params.has(`quantity${i}`)) {
-                        a_qty = params.get(`quantity${i}`);
-                        // make textboxes sticky in case of invalid data
-                        product_selection_form[`quantity${i}`].value = a_qty
-                        total_qty += a_qty;
-                        if (!isNonNegativeInteger(a_qty)) {
-                            has_errors = true; // if invalid quantity
-                 checkQuantityTextbox(product_selection_form[`quantity${i}`]); // shows where the error is
-                        }
-                    }
-                }
-                
-                console.log(Date.now() + ': Purchase made from ip ' + req.ip + ' data: ' + JSON.stringify(req.query)); //log purchase quantities
-            }
-            next();
-        }
-        
-        return str;
-    }
-
-
- 
-// routing
-// to monitor all process requests    
+// monitor all requests
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
-app.post('/process_invoice', function (request, response, next) {
-//to validate data
-let POST = request.body;
-    let name = products[0]['name'];
-    let name_price = products[0]['price'];
-
-    if (typeof POST['quantity_textbox'] != 'undefined') {
-    let quantity = POST['quantity_textbox'];
-    if (isNonNegativeInteger(quantity)) {
-        products[0]['total_qty'] += Number(quantity);
-        response.send(`<H2>Thank you for ordering ${quantity} ${name}! Your total is \$${quantity * name_price}.</H2>`);
-    }else {
-        response.send(`<I>${quantity} is not a valid quantity!</I>`);
-    }
-}
-
-// to validate that an input value = a non negative integer
-// inputstring is the input string; returnErrors indicates how the function returns
-// true = return the array, false = return a boolean.    
-function isNonNegInt(inputstring, returnErrors = false) {
-    errors = []; // assume no errors at first
-    if (Number(inputstring) != inputstring) {
-        errors.push('Not a number!'); // this is to check if string = a number value
-    }
-    else {
-        if (inputstring > 10) errors.push('<font color="red">Only 10 items in stock!</font>'); // Checks if qty is within the available products
-        if (inputstring == 0) errors.push('<font color="red">Enter a Number!</font>') // Checks if it is a Number
-        if(inputstring < 0) errors.push('<font color="red">Negative value!</font>'); // Check if it is non-negative
-        if(parseInt(inputstring) != inputstring) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer
-        
-        else if (q > products[i].quantity_available) errors.push('<font color="red">Over quantity available!</font>'); //Checks the available quantity
-
-    }
-    return returnErrors ? errors : (errors.length == 0);
-}
-//error bag
-var errors={};
-
-
-
-// route all other GET requests to files in public 
-
-//if the data is valid, send them to the invoice, otherwise send them back to products_display
-if(Object.keys(errors).length == 0) {
-    response.redirect('./invoice.html?'+ qs.stringify(request.body)); //move to invoice page if no errors
-}else{
-    response.redirect('/products_display?'+ qs.stringify(request.body));
-}
+products.forEach((products, i) => { products.total_sold = 0 }); // IR1 OPTIONAL (From Lab 13; info_server_new.js)
+// server is getting products data from the product_data.json and outputting it as a js file to the html page
+// server is getting products data from the product_data.json and outputting it as a js file to the html page
+app.get('/product_data.js', function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
 });
+// need to move into /invoice to create messages
+function isNonNegInt(queryString, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if (Number(queryString) != queryString) {
+        errors.push('Not a number!'); // Check if string is a number value
+    } else {
+        if (queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
+    }
+    if (returnErrors) {
+        return errors;
+    } else if (errors.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
+/// process purchase request (validate quantities, check quantity available) taken from lab13 info server ex4, and info_server_new, this was also from help from Professor Kazman  
+app.post("/invoice.html", function (request, response) {
+    let valid = true; // if the quantity is a true number then it is valid 
+    let ordered = ""; // creating a string 
+    let goodnum = true; // if the number is valid then it is good
+    let error = "";
+    for (i = 0; i < products.length; i++) {
+        let quantity_name = 'quantity' + i;
+        let qty = request.body['quantity' + i];
+        if (qty == "") continue; 
+        if (isNonNegInt(qty) && qty > 0 && Number(qty) <= products[i].quantity_available) { // if the number is a interger and the quantity is greater than zero and the number is less than or equal to the quantity available for product 
+            products[i].quantity_available -= Number(qty); // when page is refreshed quantites available updates 
+            products[i].total_sold += Number(qty);
+            ordered += quantity_name + "=" + qty + "&";
+        } else if(isNonNegInt(qty) != true) { // if the interger is not one than this is false 
+            goodnum = false;
+        } else if (Number(qty) >= products[i].quantity_available) { // if it is number enter is more than quantity available than this is false 
+            valid = false;     
+        } else if (checkQuantityTextbox(qty) == true) {
+            error = "";
+        }
+    }
+     // Errors response redirect // from Lab 13 info_server_new.js
+    if (!goodnum) { // if not a valid number redirect, then redirect to products_display.html
+        response.redirect(`products_display.html?error=Please enter valid number`);
+     } else if (!valid) { // if quantity is greater than quanitity available redirect, then redirect to products_display.html
+        response.redirect(`products_display.html?error=Please enter number equal to or less than the quantities stated available`);
+     } else if (error = "") {
+        response.redirect(`products_display.html?error=Please enter valid number`);
+     } else { // if there are none of these errors redirect to invoice page 
+        response.redirect('invoice.html?' + ordered);
+     }
+    });
 
-//start server
-// handles request for any static files
-app.use(express.static('./public'));
+// start server
 app.listen(8080, () => console.log(`listening on port 8080`));
